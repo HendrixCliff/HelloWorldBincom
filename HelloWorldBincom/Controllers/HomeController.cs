34a80ec2c09241b384a66ce32cba17eeca90c05f
Godwin Chukwuebuka Igwegbe <godwincliff10@email.com>
2026-09-11 10:39:42 +0100

    ASP.NET Core MVC Application

---
+++ HelloWorldBincom/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldBincom.Controllers;
+
+public sealed class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Error()
+    {
+        return View();
+    }
+}
--- HelloWorldBincom/Program.cs
+++ HelloWorldBincom/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello World!");
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
